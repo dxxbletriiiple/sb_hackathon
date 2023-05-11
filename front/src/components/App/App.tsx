f2ce9a9271facf893a9ae8c468cfc9992fe765a6
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-05-11 23:42:29 +0300

    edit: change item place heart color

--- front/src/components/App/App.tsx
+++ front/src/components/App/App.tsx
@@ -1,13 +1,12 @@
 import FirstScreen from '../FirstScreen';
-import { SignUp } from '../SignUp/SignUp';
-import  ItemPlace  from '../ItemPlace/ItemPlace';
+import ItemPlace from '../ItemPlace';
 import './App.module.scss';
 
 function App(): JSX.Element {
-
 	return (
 		<div>
-			{/* <SignUp /> */}
+			{/*<FirstScreen />*/}
+			<ItemPlace imageUrl='http://placekitten.com/g/200/300' title='lorem' description='lorem ipsum' isFavorite />
 		</div>
 	);
 }
--- front/src/components/ItemPlace/ItemPlace.tsx
+++ front/src/components/ItemPlace/ItemPlace.tsx
@@ -15,7 +15,7 @@ export const ItemPlace = ({ imageUrl, title, description, isFavorite }: ItemProp
 				<svg width='22' height='20' viewBox='0 0 22 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
 					<path
 						d='M10.8333 19.8792L9.2625 18.4492C3.68333 13.39 0 10.0533 0 5.95833C0 2.62167 2.62167 0 5.95833 0C7.84333 0 9.6525 0.8775 10.8333 2.26417C12.0142 0.8775 13.8233 0 15.7083 0C19.045 0 21.6667 2.62167 21.6667 5.95833C21.6667 10.0533 17.9833 13.39 12.4042 18.46L10.8333 19.8792Z'
-						fill={isFavorite ? '#FFFFFF' : '#79747E'}
+						fill={isFavorite ? '#ff6347' : '#79747E'}
 					/>
 				</svg>
 			</div>
