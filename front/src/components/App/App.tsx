250582ea8aa3ca560692f30cd535e91d905d08b1
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-05-12 04:37:54 +0300

    cring

--- front/src/components/App/App.tsx
+++ front/src/components/App/App.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useRef } from 'react';
 import FirstScreen from '../FirstScreen';
-import Footer from '../Footer/Footer';
-import ItemPlace from '../ItemPlace';
-import Register from '../Register';
+
 import './App.module.scss';
 
 function App(): JSX.Element {
