e153b804a46fff782410cccbaef1c1ba920e1381
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-05-12 04:24:43 +0300

    try: send data

--- back/index.js
+++ back/index.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
 const whitelist = ['https://sb-hackathon-28q49o88u-dxxbletriiiple.vercel.app'];
+
+let rawdata = fs.readFileSync('data.json');
+
 const corsOptions = {
 	origin: function (origin, callback) {
 		if (whitelist.indexOf(origin) !== -1) {
@@ -14,5 +18,4 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-app.listen(3000, (e) => console.log('damn' + e));
+app.get('/', (req, res) => res.send(rawdata));
